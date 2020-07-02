d28de017c45271480fbb3725d0a8329d5c61b8d9
danielcliu <dcliu@ucdavis.edu>
2020-07-02 15:37:41 -0700

    deleted bad file

--- youtube_transcript_channel_api/call.py
+++
@@ -1,8 +0,0 @@
-from _api import YoutubeChannelTranscripts
-from pprint import pprint
-
-x = YoutubeChannelTranscripts('The Great War', 'AIzaSyC_OY9wErKeHnoIGzbdNLcY4XV3jhWBFj8')
-
-vids, error = x.get_transcripts(just_text = True)
-pprint(vids)
-print('\nErrors: ', error)
